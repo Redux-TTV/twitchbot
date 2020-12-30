7fb7b6ae2545a760f75ffdb8a1462843b01144b8
redux-ttv <tehcaelkrigbaum@gmail.com>
2020-12-29 18:11:00 -0800

    changed database query to occur after the message instead of opening connection on init.

--- app.js
+++ app.js
@@ -1,56 +1,53 @@
 const tmi = require('tmi.js');
 var mongodb = require('mongodb');
 
-const MongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017/twitchbot";
-MongoClient.connect(url,(err,db) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('DB connection successful.');
-        startBot(db);
-    }
+const ch = "ctkttv";
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: 'botCTK',
+        password: 'oauth:n61c6qc1dvjo2us5c3fhpweaqtf1kt'
+    },
+    channels: [ch]
+};
+const client = new tmi.client(options);
+client.connect();
+client.on('connected', (address, port) => {
+    const messages = ["I'm baaaack", "Oh god I'm back.", "Where am I?", '*hacker voice*'+"I'm in."]
+    const messageNum = getRandomInt(messages.length-1);
+    client.action(ch, messages[messageNum]);
 });
-
-function startBot(db) {
-    const ch = "ctkttv";
-    const options = {
-        options: {
-            debug: true
-        },
-        connection: {
-            cluster: "aws",
-            reconnect: true
-        },
-        identity: {
-            username: 'botCTK',
-            password: 'oauth:n61c6qc1dvjo2us5c3fhpweaqtf1kt'
-        },
-        channels: [ch]
-    };
-    const client = new tmi.client(options);
-    client.connect();
-    client.on('connected', (address, port) => {
-        const messages = ["I'm baaaack", "Oh god I'm back.", "Where am I?", '*hacker voice*'+"I'm in."]
-        const messageNum = getRandomInt(messages.length-1);
-        client.action(ch, messages[messageNum]);
-    });
-    client.on('chat',(channel, user, message, self) => {
-        if(message.substr(0,1)==='!') {
-            let output = db.collection('commands').find({"command":message.substr(1)}).message;
-            if(output) {
-                client.action(ch, output);
+client.on('chat',(channel, user, message, self) => {
+    if(message.substr(0,1)==='!') {
+        const MongoClient = mongodb.MongoClient;
+        const url = "mongodb://localhost:27017/twitchbot";
+        MongoClient.connect(url,(err,db) => {
+            if(err) {
+                console.log(err);
             } else {
-                client.action(ch, message + ' is not a known command.');
+                console.log('DB connection successful.');
+                let output = db.collection('commands').find({"command":message.substr(1)}).message;
+                if(output) {
+                    client.action(ch, output);
+                } else {
+                    client.action(ch, message + ' is not a known command.');
+                }
+                db.close();
             }
-        }
-        // if(message === '!triple') {
-        //     client.action(ch, 'https://www.twitch.tv/lolwutduck/clip/UninterestedFrailJellyfishSoonerLater');
-        // } else if(message === "!uhhh") {
-        //     client.action(ch,'https://clips.twitch.tv/RefinedPrettyAlpacaDogFace');
-        // }
-    });
-}
+        });
+    }
+    // if(message === '!triple') {
+    //     client.action(ch, 'https://www.twitch.tv/lolwutduck/clip/UninterestedFrailJellyfishSoonerLater');
+    // } else if(message === "!uhhh") {
+    //     client.action(ch,'https://clips.twitch.tv/RefinedPrettyAlpacaDogFace');
+    // }
+});
 
 // client.connect();
 
