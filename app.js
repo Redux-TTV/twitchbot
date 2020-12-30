bbe6605548e064fd5e1de93abaa284429167a6b7
redux-ttv <tehcaelkrigbaum@gmail.com>
2020-12-29 17:52:03 -0800

    first try querying mongo database for commands

---
+++ app.js
@@ -0,0 +1,76 @@
+const tmi = require('tmi.js');
+var mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017/twitchbot";
+MongoClient.connect(url,(err,db) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('DB connection successful.');
+        startBot(db);
+    }
+});
+
+function startBot(db) {
+    const ch = "ctkttv";
+    const options = {
+        options: {
+            debug: true
+        },
+        connection: {
+            cluster: "aws",
+            reconnect: true
+        },
+        identity: {
+            username: 'botCTK',
+            password: 'oauth:n61c6qc1dvjo2us5c3fhpweaqtf1kt'
+        },
+        channels: [ch]
+    };
+    const client = new tmi.client(options);
+    client.connect();
+    client.on('connected', (address, port) => {
+        const messages = ["I'm baaaack", "Oh god I'm back.", "Where am I?", '*hacker voice*'+"I'm in."]
+        const messageNum = getRandomInt(messages.length-1);
+        client.action(ch, messages[messageNum]);
+    });
+    client.on('chat',(channel, user, message, self) => {
+        if(message.substr(0,1)==='!') {
+            let output = db.collection('commands').find({"command":message.substr(1)}).message;
+            if(output) {
+                client.action(ch, output);
+            } else {
+                client.action(ch, message + ' is not a known command.');
+            }
+        }
+        // if(message === '!triple') {
+        //     client.action(ch, 'https://www.twitch.tv/lolwutduck/clip/UninterestedFrailJellyfishSoonerLater');
+        // } else if(message === "!uhhh") {
+        //     client.action(ch,'https://clips.twitch.tv/RefinedPrettyAlpacaDogFace');
+        // }
+    });
+}
+
+// client.connect();
+
+// client.on('connected', (address, port) => {
+//     const messages = ["I'm baaaack", "Oh god I'm back.", "Where am I?", '*hacker voice*'+"I'm in."]
+//     const messageNum = getRandomInt(messages.length-1);
+//     client.action(ch, messages[messageNum]);
+    
+// });
+
+// client.on('chat',(channel, user, message, self) => {
+    
+//     if(message === '!triple') {
+//         client.action(ch, 'https://www.twitch.tv/lolwutduck/clip/UninterestedFrailJellyfishSoonerLater');
+//     } else if(message === "!uhhh") {
+//         client.action(ch,'https://clips.twitch.tv/RefinedPrettyAlpacaDogFace');
+//     }
+// });
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
