b5ab755ffab1fd795b4208364b47b7b64ec90fda
redux-ttv <tehcaelkrigbaum@gmail.com>
2020-12-30 14:22:09 -0800

    added debug statement

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ client.on('connected', (address, port) => {
     client.action(ch, messages[messageNum]);
 });
 client.on('chat',(channel, user, message, self) => {
+    console.log(user);
     if(message.substr(0,1)==='!' && !message.startsWith('!addcommand')) {
         const MongoClient = mongodb.MongoClient;
         const url = "mongodb://localhost:27017/twitchbot";
